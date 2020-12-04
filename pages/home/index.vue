39dc47c4dbb03274e7f089176b5fd1ed91369cda
lion <12691282@qq.com>
2020-12-04 18:00:29 +0800

    首页微调

---
+++ pages/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="wrap">
+		我的首页
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+	
+		},
+		methods: {
+		 
+		}
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,12 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="wrap">
+		<view class="u-page">
+			<u-swiper :list="list" :effect3d="true" :title="true"></u-swiper>
+			<u-line color="green" margin="20rpx" border-style="dashed"/>
 		</view>
+		<!-- 与包裹页面所有内容的元素u-page同级，且在它的下方 -->
+		<u-tabbar v-model="current" :list="tablist" :border-top="false" @change="clickButton()">
+		</u-tabbar>
 	</view>
 </template>
 
@@ -11,14 +14,65 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				list: [{
+						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					  },
+					  {
+						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					  },
+					  {
+						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}],
+				tablist: [{
+							iconPath: "home",
+							selectedIconPath: "home-fill",
+							text: '首页',
+							path: '/pages/home/index',
+							isDot: true,
+							customIcon: false,
+						},
+						{
+							iconPath: "photo",
+							selectedIconPath: "photo-fill",
+							text: '放映厅',
+							path: '/pages/home/index',
+							isDot: true,
+							customIcon: false,
+						},
+						{
+							iconPath: "play-right",
+							selectedIconPath: "play-right-fill",
+							text: '直播',
+							path: '/pages/home/index',
+							isDot: true,
+							customIcon: false,
+						},
+						{
+							iconPath: "account", 
+							selectedIconPath: "account-fill",
+							text: '我的',
+							path: '/pages/home/index',
+							isDot: false,
+							customIcon: false,
+						},
+					],
+					current: 0
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			clickButton(){
+				var path = this.tablist[this.current].path
+				this.$u.route({
+					url: path
+				})
+			}
+			
 		}
 	}
 </script>
